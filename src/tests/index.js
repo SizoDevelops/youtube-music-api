293e610aee45f9839060a3e964f7e1898ca41d65
SizoDevelops <fierceincii@gmail.com>
2025-06-28 16:51:43 +0200

    Fixed Axios Defaults

--- src/tests/index.js
+++ src/tests/index.js
@@ -3,6 +3,8 @@ const YoutubeMusicApi = require('../index.js')
 const api = new YoutubeMusicApi()
 api.initalize() // Retrieves Innertube Config
 .then(info => {
-	api.search("ne deve ne kush", "song").then(result => {}) // just search for songs
+	api.search("Uvume Kanjani", "song").then(result => {
+		console.log(result)
+	}) // just search for songs
 	
 })
